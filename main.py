7fd4004624417c1e820fdf6348e3fccd71c6d35a
AlexNazaroff <ale.krotov@gmail.com>
2021-03-15 07:25:36 +0000

    Add Firefox Webdriver and find_element_by_name

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 ffPath =="C:\Users\admin.admin2\AppData\Local\Programs\Python\Python37\Lib\site-packages\selenium\webdriver\firefox\geckodriver.exe"
 os.environ["webdriver.firefox.driver"] = ffPath
-driver = webdriver.Firefox(executable_path=ffPath)
-driver.get(url)
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+browser = webdriver.Chrome()
+
+browser.get('http://www.yahoo.com')
+assert 'Yahoo' in browser.title
+
+elem = browser.find_element_by_name('p')  # Find the search box
+elem.send_keys('seleniumhq' + Keys.RETURN)
