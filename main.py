84ed5085ebcaf1c320443ac72c6cee2ddd25c3f3
AlexNazaroff <ale.krotov@gmail.com>
2021-03-15 07:29:47 +0000

    elem.send_keys

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-ffPath =="C:\Users\admin.admin2\AppData\Local\Programs\Python\Python37\Lib\site-packages\selenium\webdriver\firefox\geckodriver.exe"
-os.environ["webdriver.firefox.driver"] = ffPath
-
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
